7fa702eee668a81ecf134efd6047862c48278e0c
Chema Sanchez <josemaria.chauchina@gmail.com>
2025-11-14 22:26:25 +0100

    Cambios home

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,28 @@ $(function () {
 
     $(window).on('load', function () {
         const elapsed = Date.now() - startTime;
-        // const remaining = Math.max(3800 - elapsed, 0);
-        const remaining = Math.max(0 - elapsed, 0);
+        const remaining = Math.max(3800 - elapsed, 0);
 
         setTimeout(() => {
-            $('#video-container-loading').fadeOut(500, function () {
-                $('main#page').fadeIn(300);
-            });
+            $('#video-container-loading').fadeOut(500);
+            $('main#page').fadeIn(100);
+
+            setTimeout(() => {
+                const msnry = new Masonry('.grid', {
+                    itemSelector: '.grid-item',
+                    columnWidth: '.grid-sizer', // usa grid-sizer como referencia
+                    percentPosition: true,
+                    gutter: 0
+                });
+
+                // Recalcular después de que los vídeos carguen
+                const videos = document.querySelectorAll(".grid video");
+                videos.forEach(video => {
+                    video.addEventListener("loadedmetadata", () => {
+                        msnry.layout();
+                    });
+                });
+            }, 310);
         }, remaining);
     });
 });
